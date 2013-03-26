1ea526932db23db85f9ef634e51c4190f9a304d1
Dan Corkill <daniel.corkill@gmail.com>
2013-03-26 12:18:50 +0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hawk::Application.config.secret_token = 'c0ed2ea6425d7bcc59e4f2e1a3ac29897b079f31d480dcee4ca36b7fe2823b913497b8091eb7386177ee8d3bf22e7deb54d1391a61a9e3dcb8d3d9bf9e6aff21'
